cbcdc64b75032319d1c7b85317db5b5ee9afadaa
HarmishaPrajapati <harmisha.prajapati97@gmail.com>
2019-04-09 14:29:05 +0530

    new changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.action_mailer.default_url_options = { host: 'templatedemo.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'templatedemo.com',
+  domain:               'localhost',
   user_name:            'noreplydumy@gmail.com',
   password:             'noreply8080',
   authentication:       'plain',
